3db6e4041c1391847f75e229ecf567ffa2661837
PTRL480\Vyshnavi.Gubala <vyshnavi.m@yesgnome.com>
2022-02-24 21:47:02 +0530

    Created bullet prefab and added material to it and also instantiated bullet prefab.

--- Assets/BulletLauncher.cs
+++ Assets/BulletLauncher.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class BulletLauncher : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public GameObject enemyPrefab;
     public int speed;
+    public float time;
     
     // Start is called before the first frame update
     void Start()
@@ -27,6 +29,15 @@ public class BulletLauncher : MonoBehaviour
 
             tempBullet.GetComponent<Rigidbody>().velocity = Camera.main.transform.rotation*Vector3.forward*speed;
 
+        }
+        time=time+Time.deltaTime;
+        if(time>=4.0f)
+        {
+            float x = Random.Range(-10.0f, 10.0f);
+            float y= Random.Range(0.0f, 1.5f);
+            float z = Random.Range(-10.0f, 10.0f);
+            Instantiate(enemyPrefab, new Vector3(x, y, z),Quaternion.identity);
+            time = 0.0f;
         }
 
     }
